131aeb114714c70418a60f3a15b57ada31e1e1f8
Md Ashikur Rahman <alashik194@gmail.com>
2024-03-31 15:24:01 +0600

    SSO form Design with redirect uri add max 3, API form Design, layout without protected

--- app/layout.js
+++ app/layout.js
@@ -19,47 +19,12 @@ export default function RootLayout({ children }) {
   const [toggleValue, setToggleValue] = useState(false)
   const handleToggleValue = (toggle) => {
     setToggleValue(toggle)
-    // console.log(toggle)
   }
-  console.log(toggleValue)
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div id="app-wrapper" className="flex">
-          
-          {/* <Sidebar toggleValue={toggleValue} /> */}
-
-
-          <div className={`${toggleValue?'min-w-[80px]':'w-[80px] sm:min-w-[260px]'} app-menu `}>
-
-<a className='logo-box' href='index.html'>
-    <img src="assets/images/logo-light.png" className="logo-light h-6" alt="Light logo"/>
-    <img src="assets/images/logo-dark.png" className="logo-dark h-6" alt="Dark logo"/>
-</a>
-
-{/* <!--- Menu --> */}
-<div data-simplebar>
-<ul className="menu" data-fc-type="accordion">
-    <li className="menu-title">Menu</li>
-
-    <li className="menu-item">
-        <Link className='menu-link waves-effect' href='/'>
-            <span className="menu-icon"><i className="ph-duotone ph-house"></i></span>
-            <span className={`${toggleValue?'hidden':'hidden sm:block'} `}> Dashboard </span>
-            {/* <span className={`${toggleValue?'hidden':'block'} badge bg-primary rounded ms-auto`}>01</span> */}
-        </Link>
-    </li>
-
-</ul>
-</div>
-</div>
-
-
-          <div className="app-content">
-            <Header handleToggleValue={handleToggleValue} />
+        
           {children}
-          </div>
-        </div>
       </body>
     </html>
   );
