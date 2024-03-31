e030a2aef6effec12461fbb2f693fc2eabbb35b1
Md Ashikur Rahman <alashik194@gmail.com>
2024-03-31 16:07:59 +0600

    login changes

--- app/clients/layout.js
+++ app/clients/layout.js
@@ -13,32 +13,32 @@ function layout({children}) {
     <>
     <div id="app-wrapper" className="flex">
           
-          {/* <Sidebar toggleValue={toggleValue} /> */}
+        {/* <Sidebar toggleValue={toggleValue} /> */}
 
 
-          <div className={`${toggleValue?'min-w-[80px]':'w-[80px] sm:min-w-[260px]'} app-menu `}>
+        <div className={`${toggleValue?'min-w-[80px]':'w-[80px] sm:min-w-[260px]'} app-menu `}>
 
         <Link className='logo-box' href='/'>
             <img src="assets/images/logo-light.png" className="logo-light h-6" alt="Light logo"/>
             <img src="assets/images/logo-dark.png" className="logo-dark h-6" alt="Dark logo"/>
         </Link>
 
-{/* <!--- Menu --> */}
-<div data-simplebar>
-<ul className="menu" data-fc-type="accordion">
-    <li className="menu-title">Menu</li>
+        {/* <!--- Menu --> */}
+            <div data-simplebar>
+                <ul className="menu" data-fc-type="accordion">
+                    <li className="menu-title">Menu</li>
 
-    <li className="menu-item">
-        <Link className='menu-link waves-effect' href='/'>
-            <span className="menu-icon"><i className="ph-duotone ph-house"></i></span>
-            <span className={`${toggleValue?'hidden':'hidden sm:block'} `}> Dashboard </span>
-            {/* <span className={`${toggleValue?'hidden':'block'} badge bg-primary rounded ms-auto`}>01</span> */}
-        </Link>
-    </li>
+                    <li className="menu-item">
+                        <Link className='menu-link waves-effect' href='/'>
+                            <span className="menu-icon"><i className="ph-duotone ph-house"></i></span>
+                            <span className={`${toggleValue?'hidden':'hidden sm:block'} `}> Dashboard </span>
+                            {/* <span className={`${toggleValue?'hidden':'block'} badge bg-primary rounded ms-auto`}>01</span> */}
+                        </Link>
+                    </li>
 
-</ul>
-</div>
-</div>
+                </ul>
+            </div>
+        </div>
 
 
           <div className="app-content">
