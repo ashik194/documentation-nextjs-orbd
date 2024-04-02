442a1e03ff21e1db8dc11bd01e7ae95a5df9b68f
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-04-02 11:29:48 +0600

    sigup and login route updated

---
+++ app/login/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from '../components/authentication/login'
+
+
+function Login() {
+  return (
+    <>
+    <LoginForm />
+    </>
+  )
+}
+
+export default Login
