78097c4f04eec017dfa723def4e4faadc133fe05
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-04-03 14:04:12 +0600

    admin dashboard design update

--- app/components/adminDashabordView.js
+++ app/components/adminDashabordView.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function AdminDashboard({children}) {
+export default function AdminDashboard({ children }) {
   return (
-
-  <div className='min-w-screen min-h-screen p-4 bg-[#F3F3F9]'>
-   {children}
-  </div>
+    <>
+      <div className='px-4 py-2 shadow-md bg-white'>
+        <h4 className='text-[15px] font-bold uppercase '>Admin Dashboard</h4>
+      </div>
+      <div className='min-w-screen min-h-screen p-4 bg-[#F3F3F9]'>
+        {children}
+      </div>
+    </>
   )
 }
