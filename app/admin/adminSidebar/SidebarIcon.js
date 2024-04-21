38a3cb988b9626f52085df77f94e6656b2d21856
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-04-21 15:11:13 +0600

    Admins - sidebar icons removed

--- app/admin/adminSidebar/SidebarIcon.js
+++ app/admin/adminSidebar/SidebarIcon.js
@@ -24,11 +24,12 @@ function SidebarIcon({ toggleValue, onUpdateSelected  }) {
 
         <div className='hover:cursor-pointer flex flex-col gap-3'>
 
-          <div className={
+          <Link className={
             selected==='dashboard'? 'h-10 w-10 bg-[#DFF1FB] rounded flex items-center justify-center'
             :'h-10 w-10 rounded flex items-center justify-center'} 
             id='dashboard'
-            onClick={()=>handleSelectedChange('dashboard')}>
+            onClick={()=>handleSelectedChange('dashboard')}
+            href={'/admin'}>
             {
                 selected === 'dashboard' ? (
                   <RiDashboard2Line style={{ color: '#25a0e2' }} size={20} />
@@ -36,35 +37,7 @@ function SidebarIcon({ toggleValue, onUpdateSelected  }) {
                   <RiDashboard2Line style={{ color: '#6D7080' }} size={20} />
                 )
               }
-          </div>
-
-          <div className={
-            selected==='app'? 'h-10 w-10 bg-[#DFF1FB] rounded flex items-center justify-center'
-            :'h-10 w-10 rounded flex items-center justify-center'} 
-            id='app'
-            onClick={()=>handleSelectedChange('app')}>
-            {
-                selected === 'app' ? (
-                  <RiApps2Line style={{ color: '#25a0e2' }} size={20} />
-                ) : (
-                  <RiApps2Line style={{ color: '#6D7080' }} size={20} />
-                )
-              }
-          </div>
-
-          <div className={
-            selected==='layout'? 'h-10 w-10 bg-[#DFF1FB] rounded flex items-center justify-center'
-            :'h-10 w-10 rounded flex items-center justify-center'} 
-            id='layout'
-            onClick={()=>handleSelectedChange('layout')}>
-            {
-                selected === 'layout' ? (
-                  <RiLayout2Line style={{ color: '#25a0e2' }} size={20} />
-                ) : (
-                  <RiLayout2Line style={{ color: '#6D7080' }} size={20} />
-                )
-              }
-          </div>
+          </Link>
 
         </div>
 
